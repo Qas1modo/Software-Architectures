8bcd405b8acb1cec86885302cf81225366e53159
Josef Zizka <josefzizka21@gmail.com>
2025-01-30 00:52:06 +0100

    fix syntax error

--- HotelSystem/RoomManagementSystem/RoomManagementSystem.BL/Queries/ReservationQueries/GetReservationsQuery.cs
+++ HotelSystem/RoomManagementSystem/RoomManagementSystem.BL/Queries/ReservationQueries/GetReservationsQuery.cs
@@ -4,6 +4,4 @@ using RoomManagementSystem.Contracts;
 namespace RoomManagementSystem.BL.Queries.ReservationQueries;
 
 public record GetReservationsQuery(DateTime From, DateTime To, int Page = -1, int PageSize = -1) : IRequest<IList<ReservationDetailModel>>;
-{
 
-}
