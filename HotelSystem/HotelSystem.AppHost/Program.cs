8437ca8c250aaaa176518cbeb4b055a7817ba575
xkosvan <xkosvan@fi.muni.cz>
2025-01-30 20:57:26 +0100

    Fix: Sln

--- HotelSystem/HotelSystem.AppHost/Program.cs
+++ HotelSystem/HotelSystem.AppHost/Program.cs
@@ -3,5 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 builder.AddProject<Projects.HotelServiceSystem_Api>("hotelservicesystem-api");
 builder.AddProject<Projects.BillingSystem>("billingsystem");
 builder.AddProject<Projects.AccessSystem_Api>("accesssystem-api");
+builder.AddProject<Projects.RoomManagementSystem_API>("roommanagementsystem-api");
 
 builder.Build().Run();
