5ee0017e2d22e5bd024f6767bbf847f1e8604aac
Josef Zizka <josefzizka21@gmail.com>
2025-01-30 15:24:10 +0100

    changed model type

--- HotelSystem/RoomManagementSystem/RoomManagementSystem.BL/Queries/ReservationQueries/GetReservationsQuery.cs
+++ HotelSystem/RoomManagementSystem/RoomManagementSystem.BL/Queries/ReservationQueries/GetReservationsQuery.cs
@@ -3,5 +3,5 @@ using RoomManagementSystem.Contracts;
 
 namespace RoomManagementSystem.BL.Queries.ReservationQueries;
 
-public record GetReservationsQuery(DateTime From, DateTime To, int Page = -1, int PageSize = -1) : IRequest<IList<ReservationDetailModel>>;
+public record GetReservationsQuery(DateTime From, DateTime To, int Page = -1, int PageSize = -1) : IRequest<IList<ReservationListModel>>;
 
